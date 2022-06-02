5227f043fe4f14b2310b07428fc35c96052020f7
Eugene Borowski <E.Borovskiy@yandex.ru>
2022-06-02 16:18:00 +0300

    Добавляет, но пока без каринки

---
+++ GB_MVCCourse/Controllers/CatalogController.cs
@@ -0,0 +1,29 @@
+﻿using GB_MVCCourse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GB_MVCCourse.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private static Catalog _catalog = new();
+
+        [HttpGet]
+        public IActionResult Products()
+        {
+            return View(_catalog);
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product model)
+        {
+            _catalog.Products.Add(model);
+            return View(_catalog);
+        }
+    }
+}
---
+++ GB_MVCCourse/Models/Catalog.cs
@@ -0,0 +1,14 @@
+﻿namespace GB_MVCCourse.Models
+{
+    public class Catalog
+    {
+        public List<Product> Products { get; set; } = new();
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Image { get; set; }
+    }
+}
